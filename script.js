be1287c036886a7c66514f42a4ac1799441aec74
31ras <vinieiras2013@gmail.com>
2025-03-23 08:15:01 -0300

    4th commit
    This time i added the password verification.

---
+++ script.js
@@ -0,0 +1,13 @@
+function verify() {
+    const password = document.getElementById("new-password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+    const result = document.getElementById("result");
+    
+    if (password === confirmPassword) {
+        result.innerText = "Passwords Match!";
+        result.style.color = "green";
+    } else {
+        result.innerText = "Passwords Do Not Match";
+        result.style.color = "red";
+    }
+}
